900bb3fb30aed014b675973ef3250cda74caafab
Jared Barnes <jared-barnes@leavitt.com>
2017-10-03 01:40:03 -0600

    Cleaned App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import Home from "./scenes/Home";
 import BrowseRestaurants from "./scenes/BrowseRestaurants";
 import Navigation from "./layouts/Navigation";
-import Tabs from "./layouts/Tabs";
-import Stars from "./layouts/Stars";
-import Rating from "./layouts/Rating";
-import LoadingContent from "./layouts/LoadingContent";
 import Restaurant from "./scenes/Restaurant";
 import store from "./services/singletonStore";
 
@@ -31,14 +27,7 @@ class App extends Component {
       <Navigation style={{ width: "100%", height: "100%" }} ref={nav => this.navigation = nav}>
         <Home name="Home" />
         <BrowseRestaurants name="BrowseRestaurants" />
-        <Tabs name="Tabs" >
-          <div name={"One"}>One</div>
-          <div name={"Two"}>Two</div>
-        </Tabs>
-        <Stars name={"Stars"} rating={5} />
-        <Rating name={"Rating"} />
         <Restaurant name={"Restaurant"} />
-        <LoadingContent name={"LoadingContent"} isLoading={true} />
       </Navigation >
     );
   }
